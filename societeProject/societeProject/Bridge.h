3b080c9f1f921f90d13c82bd70b3b0a0bde89688
lamiajrad <jrad.lamia@gmail.com>
2015-12-18 22:29:21 +0100

    http get request + Add AFNetworking+ parse json

---
+++ societeProject/societeProject/Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  societeProject
+//
+//  Created by Lamia Jrad on 18/12/2015.
+//  Copyright © 2015 Lamia Jrad. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+
+#endif /* Bridge_h */
